327d3649b5e138e665ae37bc1e4805003878de06
adastraz <tylergosox@gmail.com>
2020-05-14 20:23:30 -0400

    mvp

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -19,16 +19,17 @@ class BSTNode:
     def insert(self, value):
         if not self.value:
             self.value = value
-        if value >= self.value:
+        elif value >= self.value:
             if not self.right:
                 self.right = BSTNode(value)
-                return True
-            return self.right.insert(value)
+            else:
+                self.right.insert(value)
+
         else:
             if not self.left:
                 self.left = BSTNode(value)
-                return True
-            return self.left.insert(value)
+            else:
+                self.left.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
@@ -55,29 +56,57 @@ class BSTNode:
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        fn(self.value)
         if self.right:
             self.right.for_each(fn)
         if self.left:
             self.left.for_each(fn)
-        fn(self.value)
 
-
+    def iterative_for_each(self, fn):
+        stack = []
+        stack.append(self)
+        while len(stack) > 0:
+            current = stack.pop()
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
+            fn(current.value)
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if self.left: 
+            self.left.in_order_print(self.left)
+        print(self.value)
+        if self.right: 
+            self.right.in_order_print(self.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        if node == None:
+            return
+        queue = []
+        # Appends itself
+        queue.append(node)
+        while(len(queue) > 0):
+            print(queue[0].value)
+            current = queue.pop(0)
+            if current.left != None:
+                queue.append(current.left)
+            if current.right != None:
+                queue.append(current.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        print(node.value)
+        if self.left:
+            self.left.dft_print(self.left)
+        if self.right:
+            self.right.dft_print(self.right)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
